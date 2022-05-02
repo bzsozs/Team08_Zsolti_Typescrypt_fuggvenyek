a4696746aaace57bbc4a7ec651754653b2c510dc
bzsozs <zsolt.boti73@gmail.com>
2022-05-02 19:10:41 +0200

    ujabb feladat

---
+++ fuggvenyesFeladatok.js
@@ -0,0 +1,6 @@
+function TeglalapKeruletTerulet(a, b) {
+    var kerulet = 2 * (a + b);
+    var terulet = a * b;
+    document.write("<br>A ".concat(a, ",").concat(b, " oldal\u00FA t\u00E9glalap ker\u00FClete: ").concat(kerulet));
+    document.write("<br>A ".concat(a, ",").concat(b, " oldal\u00FA t\u00E9glalap ter\u00FClete: ").concat(terulet));
+}
---
+++ fuggvenyesFeladatok.ts
@@ -0,0 +1,7 @@
+function TeglalapKeruletTerulet(a: number, b: number): void {
+
+    let kerulet: number = 2 * (a + b);
+    let terulet: number = a * b;
+    document.write(`<br>A ${a},${b} oldalú téglalap kerülete: ${kerulet}`);
+    document.write(`<br>A ${a},${b} oldalú téglalap területe: ${terulet}`);
+}
